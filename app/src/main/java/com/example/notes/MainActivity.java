2e8dc02c5eb8c2011b78b0499c340059769cd006
Dzianis Yauseyenka <dzianis.yauseyenka@unilodz.eu>
2020-09-08 09:59:06 +0200

    Added new activity

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -14,22 +18,32 @@ public class MainActivity extends AppCompatActivity
     private RecyclerView recyclerViewNotes;
 
     ///create ArrayList,who will contain notes
-    private ArrayList<Note> notes = new ArrayList<>();
+    public static final ArrayList<Note> notes = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
-        notes.add(new Note("Brush","make","Monday",2));
-        notes.add(new Note("Football","play","Friday",1));
-        notes.add(new Note("Hokey","play","Sunday",3));
-        NotesAdapter adapter = new NotesAdapter(notes);
 
+        if(notes.isEmpty())
+        {
+            notes.add(new Note("Brush", "make", "Monday", 2));
+            notes.add(new Note("Football", "play", "Friday", 1));
+            notes.add(new Note("Hokey", "play", "Sunday", 3));
+        }
+        NotesAdapter adapter = new NotesAdapter(notes);
         ///set recyclerView Layout like Linear
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
 
         ///set adapter
         recyclerViewNotes.setAdapter(adapter);
+    }
+
+    public void onClickAdd(View view)
+    {
+        Intent intent = new Intent(this,AddNoteActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/notes/NotesAdapter.java
+++ app/src/main/java/com/example/notes/NotesAdapter.java
@@ -1,5 +1,6 @@
 package com.example.notes;
 
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+
+import static android.content.Intent.getIntent;
 
 public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHolder>
 {
@@ -33,14 +36,30 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NotesViewHol
     {
         ///get our note
         Note note = notes.get(position);
-
         ///set data
         notesViewHolder.textViewTitle.setText(note.getTitle());
         notesViewHolder.textViewDescription.setText(note.getDescription());
-        notesViewHolder.textViewDayOfWeek.setText(note.getDayOfWeek());
-        notesViewHolder.textViewPriority.setText(String.format("%s",note.getPriority()));
+        notesViewHolder.textViewDayOfWeek.setText(R.string.day_of_week+note.getDayOfWeek());
+        int colorId = 1;
+        int priority;
+
+        priority = note.getPriority();
+        switch (priority)
+        {
+            case 1:
+                colorId = notesViewHolder.itemView.getResources().getColor(android.R.color.holo_red_light);
+                break;
+            case 2:
+                colorId = notesViewHolder.itemView.getResources().getColor(android.R.color.holo_orange_light);
+                break;
+            case 3:
+                colorId = notesViewHolder.itemView.getResources().getColor(android.R.color.holo_green_light);
+                break;
+        }
+        notesViewHolder.textViewTitle.setBackgroundColor(colorId);
 
     }
+
 
     @Override
     public int getItemCount()
